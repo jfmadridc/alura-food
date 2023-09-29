b25147d359897497d2872afcc80d7ae8d9ea7081
JuanF Madrid <jfm@jfmadrid.com>
2023-09-29 09:14:47 -0500

    using classes and this.state

--- src/Form/DatosUsuario/index.js
+++ src/Form/DatosUsuario/index.js
@@ -2,6 +2,20 @@ import React from "react";
 import { TextField, Button, Box } from "@mui/material";
 
 class DatosUsuario extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: {
+				value: "",
+				valid: true,
+			},
+			password: {
+				value: "",
+				valid: true,
+			},
+		};
+	}
+
 	render() {
 		return (
 			<Box
@@ -13,22 +27,32 @@ class DatosUsuario extends React.Component {
 					justifyContent: "center",
 					flexDirection: "column",
 				}}
+				onSubmit={(e) => {
+					e.preventDefault();
+					console.log(this.state);
+				}}
 			>
 				<TextField
-					label="Correo electrónico"
+					label="Email"
 					variant="outlined"
 					fullWidth
 					margin="dense"
 					type="email"
 					error={false}
-					helperText={false && "Ingresa un correo electrónico válido"}
+					helperText={false && "Please enter a valid email"}
+					value={this.state.email.value}
+					onChange={(e) => this.setState({ email: { value: e.target.value } })}
 				/>
 				<TextField
-					label="Contraseña"
+					label="Password"
 					variant="outlined"
 					fullWidth
 					margin="dense"
 					type="password"
+					value={this.state.password.value}
+					onChange={(e) =>
+						this.setState({ password: { value: e.target.value } })
+					}
 				/>
 				<Button variant="contained" type="submit">
 					Siguiente
